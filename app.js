dc5adefb7be623aefc02345acc26e82e49792626
ne_uchi <drmor107@gmail.com>
2026-06-15 22:49:56 +0500

    conected files, added info and links in all pages, also added 404 page

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact-me');
+});
+
+app.use((req, res) => {
+  res.status(404).render('404');
+});
